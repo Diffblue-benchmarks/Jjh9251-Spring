6637e1eb95a001ab27513d4cdf4cab26d4aea252
GD3 <GD3@DESKTOP-602ML0A>
2019-05-23 19:25:09 +0900

    홈 컨트롤러 수정

--- src/main/java/com/navar/jin/HomeController.java
+++ src/main/java/com/navar/jin/HomeController.java
@@ -1,39 +1,42 @@
 package com.navar.jin;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
+@RequestMapping("/board")
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	String name;
+	
+	@RequestMapping("/select")
+	public String select() {
+		logger.info("Welcome home! The client locale is {}.", 1);
 		return "home";
+		
+	}
+	@RequestMapping("/insert")
+	public String insert() {
+		logger.info("Welcome home! The client locale is {}.", 1);
+		return "home";
+		
+	}
+	@RequestMapping("/update")
+	public String update() {
+		logger.info("Welcome home! The client locale is {}.", 1);
+		return "home";
+		
+	}
+	@RequestMapping("/delete")
+	public String delete() {
+		logger.info("Welcome home! The client locale is {}.", 1);
+		return "home";
+		
 	}
 	
 }
